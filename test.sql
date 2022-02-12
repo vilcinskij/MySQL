e8d7c7bf9111aa9a178062ffe96822d2dadaa097
vilcinskij <vilcinskij@gmail.com>
2022-02-12 19:09:07 +0200

    operatoriai

--- test.sql
+++ test.sql
@@ -3,7 +3,11 @@
 -- where Population>500000 and (CountryCode='AUS' or CountryCode='ARG')
 -- order by Population desc
 
-SELECT * 
-FROM country
-where Continent = 'Africa' and IndepYear = 1975;
-select * from city
+-- SELECT * 
+-- FROM country
+-- where Continent = 'Africa' and IndepYear = 1975;
+-- select * from city
+
+SELECT *
+FROM city
+WHERE name LIKE 'Vilnius'

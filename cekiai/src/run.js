058e0f4ed2079e18bbab162ca3b8cf9fd96ec2cd
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-05-07 01:34:48 +0300

    packages

--- cekiai/src/run.js
+++ cekiai/src/run.js
@@ -1,1 +1,29 @@
-console.log('labas');
+import express from "express";
+import exphbs from "express-handlebars";
+
+const PORT = 8080;
+const WEB = "web";
+
+const app = express();
+app.engine(
+    "handlebars",
+    exphbs({
+        helpers: {
+            eq(p1, p2) {
+                return p1 === p2;
+            },
+        },
+        runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+        },
+    }),
+);
+app.set("view engine", "handlebars");
+app.use(express.static(WEB));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true,
+}))
+
+app.listen(PORT);
+console.log(`Server start on port ${PORT}`);
